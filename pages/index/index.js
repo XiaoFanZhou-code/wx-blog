1588c6161e531eedcb12191634168ddbdd6c119e
zhouxiaofan <378598350@qq.com>
2021-06-23 18:04:21 +0800

    fix:完成首页部分博客列表展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,31 @@
 //index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    //轮播图配置
+    autoplay: true,
+    interval: 3000,
+    duration: 1200
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function () {
+    var that = this; 
+    var data = {
+      "datas": [
+        {
+          "id": 1,
+          "imgurl": "../../static/images/WechatIMG56.jpeg"
+        },
+        {
+          "id": 2,
+          "imgurl": "../../static/images/WechatIMG57.jpeg"
+        },
+        {
+          "id": 3,
+          "imgurl": "../../static/images/WechatIMG57.jpeg"
+        }
+      ]
+    }; 
+    that.setData({
+      lunboData: data.datas
+    })
   }
 })
