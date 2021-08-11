2aa273e3e5b1e8a27f6f091a052bddf8a6115508
NK <7658694+woaink@user.noreply.gitee.com>
2021-08-11 15:43:05 +0800

    fix:初步完成首页今天页面搭建

---
+++ config.js
@@ -0,0 +1,8 @@
+const APPID = "wxadbe315baea70ad1"; // 小程序appid
+
+const IMG_URL = "https://shengfeng-1302322675.cos.ap-shanghai.myqcloud.com/"; // static/imgs/图片地址
+
+export {
+  APPID,
+  IMG_URL,
+}
